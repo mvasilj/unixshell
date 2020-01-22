7ce317e4eb3473cd048970951c9febf2704270ed
mirkov <mvasilj93@gmail.com>
2020-01-22 15:17:58 +0100

    Zadnja verzija

--- main.c
+++ main.c
@@ -54,7 +54,11 @@ int lsh_cd(char **args)
   if (args[1] == NULL) {
     fprintf(stderr, "lsh: expected argument to \"cd\"\n");
   } else {
-    if (chdir(args[1]) != 0) {
+	if (*args[1]=='~')
+	{
+	chdir("/home");
+}
+   else if (chdir(args[1]) != 0) {
       perror("lsh");
     }
   }
